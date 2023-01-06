5a5b9f1724ec94b5582a1f87e679abfa8796cd54
Joel Bondurant <em@joelbondurant.com>
2023-01-05 21:08:15 -0600

    Jack up the security around history files with an extended hmac nonce.

--- src/appdb.rs
+++ src/appdb.rs
@@ -1,16 +1,16 @@
 use std::collections::HashMap;
 use std::time::SystemTime;
 
-use chacha20poly1305::ChaCha20Poly1305;
+use chacha20poly1305::XChaCha20Poly1305;
 use chacha20poly1305::aead::{Aead, KeyInit};
-use sha1::{Sha1, Digest};
+use sha1::Sha1;
+use sha3::Sha3_256;
 
 pub struct AppDB {
     db_map_enc: String,
     password: [u8; 32],
     db_map: HashMap<String, String>,
     db_path: String,
-    is_locked: bool,
     version: String,
 }
 
@@ -22,7 +22,6 @@ impl AppDB {
             password: [0; 32],
             db_map: HashMap::<String, String>::with_capacity(100),
             db_path: "digisafe.db".into(),
-            is_locked: true,
             version: "0000".into(),
         }
     }
@@ -39,10 +38,15 @@ impl AppDB {
     }
 
     pub fn set(&mut self, akey: String, aval: String) {
+        use sha3::Digest;
         self.unlock();
         self.db_map.insert(akey, aval);
         let dbstr = serde_json::to_string(&self.db_map).unwrap();
-        let dbstr_enc = AppDB::encrypt(dbstr, self.password);
+        let hmac: [u8; 32] = Sha3_256::digest(base64::encode(self.password) + &dbstr).try_into().unwrap();
+        let nonce: [u8; 24] = hmac[..24].try_into().unwrap();
+        let prefix = self.version.to_string() + &base64::encode(&nonce); // 4 + 32 = 36
+        assert_eq!(prefix.len(), 36);
+        let dbstr_enc = prefix + &AppDB::encrypt(dbstr, self.password, &nonce);
         self.db_map_enc = dbstr_enc;
         self.lock();
     }
@@ -59,13 +63,12 @@ impl AppDB {
             if rdb.is_ok() {
                 let raw_db = rdb.unwrap();
                 self.version = raw_db[0..4].to_string();
-                self.db_map_enc = raw_db[4..].to_string();
+                self.db_map_enc = raw_db.to_string();
                 self.unlock()
             } else {
                 "load failure E1".into()
             }
         } else {
-            self.is_locked = false;
             "unlocked".into()
         }
     }
@@ -74,13 +77,9 @@ impl AppDB {
         SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
     }
 
-    pub fn save(&mut self) -> String {
-        self.unlock();
-        let dbstr = serde_json::to_string(&self.db_map).unwrap();
-        self.lock();
-        let dbstr_enc = self.version.to_string() + &AppDB::encrypt(dbstr, self.password);
+    pub fn save(&self) -> String {
         let db_path_tmp = ".".to_string() + &self.db_path;
-        let wr1 = std::fs::write(&db_path_tmp, &dbstr_enc);
+        let wr1 = std::fs::write(&db_path_tmp, &self.db_map_enc);
         if wr1.is_ok() {
             let wr2 = std::fs::rename(&db_path_tmp, &self.db_path);
             if wr2.is_ok() {
@@ -90,7 +89,7 @@ impl AppDB {
                 if wr3.is_ok() {
                     let wr4 = std::fs::copy(&self.db_path, archive_path.join(format!("digisafe_{ts}.db")));
                     if wr4.is_ok() {
-                        let res = AppDB::upload_db(&dbstr_enc);
+                        let res = AppDB::upload_db(&self.db_map_enc);
                         if res.is_ok() {
                             "saved".into()
                         } else {
@@ -111,17 +110,22 @@ impl AppDB {
     }
 
     fn unlock(&mut self) -> String {
+        use sha3::Digest;
         self.lock();
         if self.db_map_enc == "" {
-            self.is_locked = false;
             "unlocked".into()
         } else {
-            let dbstr = AppDB::decrypt(self.db_map_enc.clone(), self.password);
+            let nonce: [u8; 24] = base64::decode(&self.db_map_enc[4..36]).unwrap().try_into().unwrap();
+            let db_map_enc = &self.db_map_enc[36..];
+            let dbstr = AppDB::decrypt(db_map_enc.into(), self.password, &nonce);
             if dbstr.is_some() {
-                let rdb: Result<HashMap<String, String>, _> = serde_json::from_str(dbstr.unwrap().as_str());
+                let dbstr = dbstr.unwrap();
+                let hmac: [u8; 32] = Sha3_256::digest(base64::encode(self.password) + &dbstr).try_into().unwrap();
+                let nonce_check: [u8; 24] = hmac[..24].try_into().unwrap();
+                assert_eq!(nonce, nonce_check);
+                let rdb: Result<HashMap<String, String>, _> = serde_json::from_str(&dbstr);
                 if rdb.is_ok() {
                     self.db_map.extend(rdb.unwrap().into_iter());
-                    self.is_locked = false;
                     "unlocked".into()
                 } else {
                     "unlock failure E2".into()
@@ -134,7 +138,6 @@ impl AppDB {
 
     fn lock(&mut self) {
         self.db_map.clear();
-        self.is_locked = true;
     }
 
     fn hash_password(password: String) -> [u8; 32] {
@@ -155,16 +158,16 @@ impl AppDB {
         hash
     }
 
-    fn encrypt(raw_text: String, key: [u8; 32]) -> String {
-        let cipher = ChaCha20Poly1305::new(&key.into());
-        let cipher_text = cipher.encrypt(&[8; 12].into(), raw_text.as_ref()).unwrap();
+    fn encrypt(raw_text: String, key: [u8; 32], nonce: &[u8; 24]) -> String {
+        let cipher = XChaCha20Poly1305::new(&key.into());
+        let cipher_text = cipher.encrypt(nonce.into(), raw_text.as_ref()).unwrap();
         base64::encode(cipher_text)
     }
 
-    fn decrypt(enc_text: String, key: [u8; 32]) -> Option<String> {
-        let cipher = ChaCha20Poly1305::new(&key.into());
+    fn decrypt(enc_text: String, key: [u8; 32], nonce: &[u8; 24]) -> Option<String> {
+        let cipher = XChaCha20Poly1305::new(&key.into());
         let blob = base64::decode(enc_text).unwrap();
-        let vplain_text = cipher.decrypt(&[8; 12].into(), blob.as_ref());
+        let vplain_text = cipher.decrypt(nonce.into(), blob.as_ref());
         if vplain_text.is_ok() {
             let plain_text = String::from_utf8(vplain_text.unwrap());
             if plain_text.is_ok() {
@@ -178,6 +181,7 @@ impl AppDB {
     }
 
     fn upload_db(dbstr_enc: &str) -> Result<String, reqwest::Error> {
+        use sha1::Digest;
         let api_config: HashMap<String, String> = serde_json::from_str(&std::fs::read_to_string("/secrets/backblaze.json").unwrap()).unwrap();
         let api_key = base64::encode(format!("{}:{}", api_config["key_id"], api_config["app_key"]));
         let auth_url = "https://api.backblazeb2.com/b2api/v2/b2_authorize_account";
@@ -196,7 +200,7 @@ impl AppDB {
         let upload_url_resp_map: HashMap<String, serde_json::Value> = serde_json::from_str(&upload_url_resp).unwrap();
         let upload_url = upload_url_resp_map["uploadUrl"].clone().as_str().unwrap().to_string();
         let upload_token = upload_url_resp_map["authorizationToken"].clone().as_str().unwrap().to_string();
-        let mut sha1_hasher: Sha1 = sha1::Sha1::new();
+        let mut sha1_hasher: Sha1 = Sha1::new();
         sha1_hasher.update(dbstr_enc.as_bytes());
         let sha1_hash = hex::encode(sha1_hasher.finalize());
         let upload_req = b2.post(upload_url).body(dbstr_enc.to_string())
